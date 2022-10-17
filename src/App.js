9e8bb136edc766aa70ccf7f5f321382a0f17fda0
Steven Rejdukowski <steveo732@gmail.com>
2022-10-17 16:12:12 -0500

    lab complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
+import Footer from './Footer.js';
 import './App.css';
 
 // class component
@@ -12,6 +13,7 @@ class App extends React.Component {
       <>
     <Header />
       <Main />
+      <Footer />
     </>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Person from './Person.js';
+import HornedBeast from './HornedBeast.js';
 
 
 class Main extends React.Component {
@@ -7,16 +7,19 @@ class Main extends React.Component {
     return (
       <>
       <main className='main-component'>
-       <Person 
-       name="Zoe" 
-       course="301"
+       <HornedBeast 
+       title="Big" 
+       alt="Big Pic"
+       imageUrl="https://images.unsplash.com/photo-1541793647037-86afaddc1cf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+       description="My boy Big looking great"
        />
-       <Person 
-       name="John"
+       <HornedBeast 
+       title="Gary"
+       alt="Pic of Gary"
+       imageUrl="https://images.unsplash.com/photo-1603298517316-fe01810dc990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+       description="Gary looking quite majestic"
        />
-       <Person 
-       name="Pork"
-       />
+
        
       </main>
       </>
