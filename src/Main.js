07187699cd2f3d97451a8c4e619f56a0f7ed023a
Steven Rejdukowski <steveo732@gmail.com>
2022-10-18 13:36:10 -0500

    first commit lab2

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import data from './data.json';
 
 
 class Main extends React.Component {
   render() {
+    let beast = []
+
+    data.forEach((thing) => {
+      beast.push(
+        <HornedBeast
+          id={thing._id}
+          imageurl={thing.image_url}
+          title={thing.title}
+          description={thing.description}
+          keyword={thing.keyword}
+          horns={thing.horns}
+         /> )
+    });
+
     return (
       <>
-      <main className='main-component'>
-       <HornedBeast 
-       title="Big" 
-       alt="Big Pic"
-       imageUrl="https://images.unsplash.com/photo-1541793647037-86afaddc1cf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-       description="My boy Big looking great"
-       />
-       <HornedBeast 
-       title="Gary"
-       alt="Pic of Gary"
-       imageUrl="https://images.unsplash.com/photo-1603298517316-fe01810dc990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-       description="Gary looking quite majestic"
-       />
-
-       
-      </main>
+        <main >
+          {beast}
+        </main>
       </>
     )
   }
